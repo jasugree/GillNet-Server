6731e5628fb8cab5f272b9d69ebb83b3949e7ed4
Jacob Greene <49542181+jasugree@users.noreply.github.com>
2021-11-14 12:47:51 -0500

    Adding Dialect

--- db.js
+++ db.js
@@ -2,7 +2,12 @@ const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL, {
 	dialect: "postgres",
-	logging: false,
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
+	},
 });
 
 module.exports = db;
